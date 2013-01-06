6c55796de70f702cd29fcb3ff38aa2e98d2c9157
Jeremy Fleischman <jeremyfleischman@gmail.com>
2013-01-06 12:41:30 -0800

    Improved ui, we now distinguish between volume config & status.

--- volumeserver.js
+++ volumeserver.js
@@ -11,10 +11,6 @@ var app = http.createServer(function(req, res){
 
 var default_config = require('./default_config');
 var custom_config = {};
-
-var default_config = require('./default_config');
-
-var custom_config = {};
 if(fs.existsSync("./config.js")) {
     custom_config = require('./config');
 }
@@ -27,47 +23,62 @@ console.log("Listening on " + config('port'));
 
 var io = require('socket.io').listen(app);
 
-var lastVolume = {
+var volumeStatus = {
 	volume: 0,
 	muted: false
 };
 
-function setVolume(newVolume) {
-	lastVolume = newVolume;
-	pushVolume();
+var volumeConfig = null;
 
+function portableSetVolume(volume, callback) {
 	// Prevent injection attacks
-	if(typeof newVolume.volume != "number") {
-		throw "volume (" + newVolume.volume + ") must be a number, not " + (typeof newVolume.volume);
+	if(typeof volume.volume != "number") {
+		callback("volume (" + volume.volume + ") must be a number, not " + (typeof volume.volume), null, null);
+        return;
 	}
-	if(typeof newVolume.muted != "boolean") {
-		throw "muted (" + newVolume.muted + ") must be a boolean, not " + (typeof newVolume.muted);
+	if(typeof volume.muted != "boolean") {
+		callback("muted (" + volume.muted + ") must be a boolean, not " + (typeof volume.muted), null, null);
+        return;
 	}
 
     if(os.platform() == "win32") {
-        var cmd = "VolCuntWin.exe " + (newVolume.volume/100.0);
-        cmd += " " + (newVolume.muted ? "mute" : "unmute");
-        exec(cmd, function(error, stdout, stderr) {
-            if(error !== null) {
-                console.log('exec error: ' + error);
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                assert(error !== null);
-            }
-        });
+        var cmd = "VolCuntWin.exe " + (volume.volume/100.0);
+        cmd += " " + (volume.muted ? "mute" : "unmute");
+        exec(cmd, callback);
     } else if(os.platform() == "linux") {
-        var cmd = "amixer set Master " + newVolume.volume + "%";
-        cmd += " " + (newVolume.muted ? "mute" : "unmute");
-        exec(cmd, function(error, stdout, stderr) {
-            if(error !== null) {
-                console.log('exec error: ' + error);
-                console.log('stdout: ' + stdout);
-                console.log('stderr: ' + stderr);
-                assert(error !== null);
-            }
-        });
+        var cmd = "amixer set Master " + volume.volume + "%";
+        cmd += " " + (volume.muted ? "mute" : "unmute");
+        exec(cmd, callback);
     } else {
         assert(false);
+    }
+}
+
+var volumeWorker = null;
+function setVolumeWorker() {
+    var volumeConfigCopy = volumeConfig;
+    portableSetVolume(
+        volumeConfigCopy, 
+        function(error, stdout, stderr) {
+            if(error !== null) {
+                console.log('exec error: ' + error);
+                console.log('stdout: ' + stdout);
+                console.log('stderr: ' + stderr);
+            }
+            if(volumeConfigCopy != volumeConfig) {
+                volumeWorker = setTimeout(setVolumeWorker, 0);
+            } else {
+                volumeConfig = null;
+                volumeWorker = null;
+            }
+        }
+    );
+}
+
+function setVolume(newVolume) {
+	volumeConfig = newVolume;
+    if(volumeWorker === null) {
+        volumeWorker = setTimeout(setVolumeWorker, 0);
     }
 }
 
@@ -111,12 +122,23 @@ function getVolume(callback) {
     }
 }
 
+function getVolumeJson(includeConfig) {
+    var json = {};
+    json.status = volumeStatus;
+    if(includeConfig) {
+        json.config = volumeConfig;
+    }
+    return json;
+}
+
 io.sockets.on('connection', function (socket) {
-	socket.emit('volume', lastVolume);
+	socket.emit('connected', socket.id);
+	socket.emit('volume', getVolumeJson(true));
 	socket.on('volume', function(data) {
 		try {
+            data.whoDidIt = this.id;
 			setVolume(data);
-			pushVolume();
+            pushVolumeConfig();
 		} catch(e) {
 			if(typeof e == "string") {
 				socket.emit('error', e);
@@ -127,21 +149,30 @@ io.sockets.on('connection', function (socket) {
 	});
 });
 
-function pushVolume() {
+function pushVolumeStatus() {
+    pushVolume(false);
+}
+function pushVolumeConfig() {
+    pushVolume(true);
+}
+
+// This is such garbage. There should be separate events
+// for config & status changes.
+function pushVolume(includeConfig) {
 	var clients = io.sockets.clients();
 	for(var i = 0; i < clients.length; i++) {
-		clients[i].emit('volume', lastVolume);
+		clients[i].emit('volume', getVolumeJson(includeConfig));
 	}
 }
 
 function pollVolume() {
 	getVolume(function(newVolume) {
-		var changed = ( ( lastVolume.volume != newVolume.volume ) ||
-						( lastVolume.muted != newVolume.muted ) );
-		lastVolume = newVolume;
+		var changed = ( ( volumeStatus.volume != newVolume.volume ) ||
+						( volumeStatus.muted != newVolume.muted ) );
+		volumeStatus = newVolume;
 		if(changed) {
 			// Since it changed, we push this out, and check again immediately.
-			pushVolume();
+			pushVolumeStatus();
 			setTimeout(pollVolume, 100);
 		} else {
 			// No change, so we'll wait a whole second before checking again.
